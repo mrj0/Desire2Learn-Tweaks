a90a947dc1abfd9a7c58a3e41c07fe8ca3d74e72
Mike Johnson <mike@mrj0.com>
2013-09-20 20:57:36 -0700

    Some tweaks
    - highlight your own messages (easier to reply to)
    - highlight the current message
    - prevent the session expired message (works unless the session is actually expired)

--- d2lfixes.user.js
+++ d2lfixes.user.js
@@ -8,12 +8,18 @@
 // @version			0.1
 // ==/UserScript==
 
-var discussRegex = new RegExp("d2l/lms/discussions(/|$)");
+var discussRegex = new RegExp("d2l/lms/discussions/");
 var emailRegex = new RegExp("d2l/lms/email(/|$)");
 
 if (document.URL.match(discussRegex)) {
+    addStylesheet();
 	processPosts();
 	processDiscussionList();
+	
+	// prevents the popup
+    setTimeout(function() {
+	    D2L.PT.Auth.SessionTimeout.m_timeoutIsHandled = true;
+    }, 5000);
 }
 
 removeLinkDecorations();
@@ -44,6 +50,18 @@ window.addEventListener('load', function () {
     setInterval(tapItLikeItsHot, POLL_INTERVAL);
 });
 
+function addStylesheet() {
+    var style = document.createElement("style");
+    // WebKit?
+    style.appendChild(document.createTextNode(""));
+    document.head.appendChild(style);
+
+    var sheet = style.sheet;
+    sheet.insertRule("#z_cg .gm-active {background-color: #99FF66; }", 0);
+    sheet.insertRule("#z_cg .gm-mine {background-color: #66CCFF;}", 0);
+
+    return style.sheet;
+};
 
 function convertToArray(domObj) {
 	return Array.prototype.slice.call(domObj, 0);
@@ -80,6 +98,23 @@ function processDiscussionList() {
 		addHighlight(target);
 		target.addEventListener('click', removeHighlight, false);
 	});
+
+    // add an event handler for each message. row clicking anything on
+    // the row changes the highlight because the d2l handler is
+    // preventing normal event bubbling, so using the tr is the
+    // easiest.
+	list = convertToArray(document.querySelectorAll('#z_cg > tbody > tr'));
+	list.forEach(function(elem) {
+	   elem.addEventListener('click', makeCurrent, false);
+	});
+
+    // search for messages the current user submitted and color them
+    var name = Global.FirstName + ' ' + Global.LastName;
+    list = convertToArray(document.querySelectorAll('#z_cg td label'));
+    list.forEach(function(elem) {
+        if(name == elem.innerHTML)
+            elem.parentNode.parentNode.classList.add('gm-mine');
+    });
 }
 
 function addHighlight (elem) {
@@ -88,4 +123,16 @@ function addHighlight (elem) {
 
 function removeHighlight() {
 	this.removeAttribute('style', 'background-color');
+}
+
+function removeCurrent() {
+	var list = convertToArray(document.querySelectorAll('#z_cg tr.gm-active'));
+	list.forEach(function(elem) {
+        elem.classList.remove('gm-active');
+    });
+}
+
+function makeCurrent(e) {
+    removeCurrent();
+    this.classList.add('gm-active');
 }
